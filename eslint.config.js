310e4d397e5488d50cc93f027a94aad985e996ff
Alessandra Renelli Jayo <alrenellij@gmail.com>
2026-04-07 21:23:45 -0400

    Update ESLint config to ignore specific file

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,17 @@
 export default [
   {
-    files: ["**/*.js"],
+    ignores: ["public/js/md5.js"]
+  },
+  {
+    files: ["public/js/*.js"],
+    languageOptions: {
+      sourceType: "script",
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        XMLHttpRequest: "readonly"
+      }
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "error"
